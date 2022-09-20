1ab66ca13a8fdf82bacb2a9840736a3b7101c1f2
unknown <ing.ramirozs@gmail.com>
2022-09-19 23:39:03 -0500

    update code

--- js/index.js
+++ js/index.js
@@ -1,35 +1,57 @@
-function encriptar (){
-    var texto = document.querySelector(".texto1").value;
-    var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.querySelector(".texto2").value = textoCifrado;
-    document.querySelector(".texto1").value;
-    }
+const Validateinput = (v) => {
+  if (v === "") {
+    document.querySelector(".span_enter_text").style.visibility = "visible";
+    setTimeout(() => {
+      document.querySelector(".span_enter_text").style.visibility = "hidden";
+    }, 900);
+  }
+  return false;
+};
 
 
-    
-var boton1 = document.querySelector(".btnencriptar");
-boton1.onclick = encriptar;
+const Encriptar = () => {
+  let texto = document.querySelector(".textarea_entertext").value;
+  Validateinput(texto)
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  document.querySelector(".textarea_result").value = textoCifrado;
+  document.querySelector(".textarea_entertext").value = "";
+};
 
+const BtnEncriptar = document.querySelector(".btnencriptar");
+BtnEncriptar.addEventListener("click", Encriptar);
 
-function desencriptar (){
-     var texto = document.querySelector(".texto1").value;
-      var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
-      document.querySelector(".texto2").value = textoCifrado;
-       document.querySelector(".texto1").value;
-
+const Desencriptar = () => {
+  let texto = document.querySelector(".textarea_entertext").value;
+  Validateinput(texto)
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  document.querySelector(".textarea_result").value = textoCifrado;
+  document.querySelector(".textarea_entertext").value = "";
 }
 
-var boton2 = document.querySelector(".btndesencriptar"); 
-boton2.onclick = desencriptar;
+const BtnDesencriptar = document.querySelector(".btndesencriptar");
+BtnDesencriptar.addEventListener("click",Desencriptar)
 
+const Copy = () => {
+  let content = document.querySelector(".textarea_result");
+  content.select();
+  navigator.clipboard.writeText(content.value);
+  content.value = "";
 
-function copyToClipBoard() {
+  document.querySelector(".span_result_copy").style.visibility = "visible";
+  setTimeout(() => {
+    document.querySelector(".span_result_copy").style.visibility = "hidden";
+  }, 550);
+}
 
-    var content = document.querySelector(".texto2");
-    content.select();
-    document.execCommand('copy');
-
-    }
-
-    var botoncopia = document.querySelector(".copia");
-    botoncopia.onclick = copyToClipBoard
+var BtnCopy = document.querySelector(".btncopia");
+BtnCopy.addEventListener("click", Copy);
